5e69d22086cf8d16dc33387659b23686590531a4
Koji <fursich0@gmail.com>
2017-01-13 15:39:12 +0000

    レストランのcrud作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170113033600) do
+ActiveRecord::Schema.define(version: 20170113140102) do
 
   create_table "genres", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170113033600) do
     t.float    "longitude"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
